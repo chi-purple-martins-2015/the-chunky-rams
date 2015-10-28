13b205ed0cf5c08c8212b86a542508f078e9ef94
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-10-28 14:15:29 -0500

    add methods to Question for points and num_answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,12 @@ class Question < ActiveRecord::Base
   has_many :tags, through: :categories
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  def points
+    self.votes.count
+  end
+
+  def num_answers
+    self.answers.count
+  end
 end
