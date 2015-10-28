691b28622125b09f8e789dfef7ba0e065e6b0993
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-10-28 14:14:13 -0500

    add question.answers.count and question.points

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,9 +1,9 @@
 get "/questions" do
-  @questions = Question.all
+  @questions = Question.find(:all, limit: 15)
   erb :"questions/index"
 end
 
 get "/questions/:id" do
-  
+
   erb :"questions/show"
 end
