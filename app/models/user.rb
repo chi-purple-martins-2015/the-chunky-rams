42cd287a11daad1da3601b5834fc6862b99bf0bf
Kyle Dolezal <email": null>
2015-10-29 18:22:42 -0500

    Compmlete changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,26 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes
 
+  def self.get_index_info
+    @user_info = []
+    User.limit(50).offset(rand(1..100)).each do |user|
+      single_user = {}
+      single_user[:id] = user.id
+      single_user[:image] = user.path_to_profile_image
+      single_user[:username] = user.username
+      single_user[:location] = "#{Faker::Address.city}, #{Faker::Address.country}"[0..30]
+      single_user [:votes] = rand(1..1200)
+      tags = []
+      rand(1..3).times do
+        tags << Tag.find_by(id: rand(1..100))
+      end
+      single_user[:tags] = tags
+      @user_info << single_user
+    end
+    @user_info
+  end
+
+
   def password
     @password ||= Password.new(hashed_password)
   end
