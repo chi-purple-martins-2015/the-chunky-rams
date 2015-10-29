a6080fba96094bff05a0096f45ad6b364ea43b32
Kyle Dolezal <email": null>
2015-10-29 18:52:08 -0500

    Fixed merge errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 enable :sessions
 
-get "/users" do
-  erb :"/users/index"
-end
+
 
 get "/users/signup" do
   @signup = true
@@ -13,6 +11,12 @@ post "/users" do
   @user = User.create!(email: params[:email], password: params[:password], username: params[:email])
   #TELL THEM ABOUT THEIR REGISTRATION ERROR
   redirect "/users/login"
+end
+
+post "/user/find" do
+  user = User.find_by(username: params['username'])
+  redirect "/users/#{user.id}" if user
+  redirect ('/users')
 end
 
 get "/users/login" do
@@ -41,20 +45,53 @@ post "/users/logout" do
   redirect "/"
 end
 
-get "/users/:id" do
-  @user = User.find_by(id: params[:id])
-  erb :"/users/profile"
-end
+
 
 #updates a specific user
 put "/users/:id" do
   #do we need this? only if user will have profile edit functionality
 end
 
+get "/users" do
+  @user = User.get_index_info
+  @active_tab = 'reputation'
+  erb :"/users/index"
+end
+
+get "/users/new" do
+  @user = User.get_index_info
+  @active_tab = 'new'
+  erb :"/users/index"
+end
+
+get "/users/voters" do
+  @user = User.get_index_info
+  @active_tab = 'voters'
+  erb :"/users/index"
+end
+
+get "/users/editors" do
+  @user = User.get_index_info
+  @active_tab = 'editors'
+  erb :"/users/index"
+end
+
+get "/users/moderators" do
+  @user = User.get_index_info
+  @active_tab = 'moderators'
+  erb :"/users/index"
+end
+
+
 delete "/users/:id" do
   @user = User.find(params[:id])
   @user.destroy
   erb :index
+end
+
+get "/users/:id" do
+  @user = User.find_by(id: params[:id])
+  erb :"/users/profile"
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+  $('.username').keypress(function(e) {
+    if (e.which == 13) {
+      $('form#user-search').submit();
+    }
+  });
   // $("#popular").fadeIn();
 
 });
